9d9482255f6f725bf8a84809b9086ee4042117a8
Jeremiah.Osidele <Jeremiah.Osidele@fdmgroup.com>
2025-11-03 19:53:49 +0000

    chore: intialise spring boot project with dependencies

---
+++ src/main/java/com/jeremiah/da_api/DaApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeremiah.da_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaApiApplication.class, args);
+	}
+
+}
