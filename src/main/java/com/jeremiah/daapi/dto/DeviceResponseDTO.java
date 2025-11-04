05b4b10f4b3b478187535fa7bd8335849f01f2cc
16ukun <136078903+16ukun@users.noreply.github.com>
2025-11-04 19:31:22 +0000

    feat: add repository and DTO (#4)
    Authored-by: Jeremiah Osidele <Jeremiahosidele@gmail.com>

---
+++ src/main/java/com/jeremiah/daapi/dto/DeviceResponseDTO.java
@@ -0,0 +1,27 @@
+package com.jeremiah.daapi.dto;
+
+import java.util.UUID;
+
+import com.jeremiah.daapi.entity.DeviceStatus;
+
+import lombok.Data;
+
+// annotation from Lombok - shortcut that automatically generates a lot of boilerplate code for you at compile time e.g. getter/setter, toString, equals, hashCode
+@Data
+public class DeviceResponseDTO {
+    private UUID deviceId;
+    private Integer assetId;
+    private String make;
+    private String model;
+    private Integer releaseYear;
+    private String os;
+    private String osVersion;
+    private String capabilities;
+    private String webdriverConfig;
+    private String region;
+    private DeviceStatus status;
+    private String raveConfig;
+    
+}
+
+
