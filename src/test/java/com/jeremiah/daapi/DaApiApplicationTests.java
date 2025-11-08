4b88a0ba7815beb9b613abef4ce9853705415871
Jeremiah.Osidele <Jeremiah.Osidele@fdmgroup.com>
2025-11-08 22:54:23 +0000

    ci: configure github workflows ci cd pipeline

---
+++ src/test/java/com/jeremiah/daapi/DaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremiah.daapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
